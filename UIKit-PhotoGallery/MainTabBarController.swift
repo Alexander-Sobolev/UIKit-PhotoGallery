dc14b92090fdf2a7619f9f5f6255f1e8938cf697
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-23 18:44:58 +0300

    Initial commit

---
+++ UIKit-PhotoGallery/MainTabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabBarController.swift
+//  UIKit-PhotoGallery
+//
+//  Created by Alexander Sobolev on 23.09.2021.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = #colorLiteral(red: 0.9484767247, green: 1, blue: 1, alpha: 1)
+    }
+    
+}
