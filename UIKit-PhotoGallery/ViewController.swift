854bb55d6ed5817193801727031bc450a475dbb2
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-10-22 20:31:55 +0300

    Network

--- UIKit-PhotoGallery/ViewController.swift
+++ UIKit-PhotoGallery/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad() 
         
         view.backgroundColor = #colorLiteral(red: 0.7351083184, green: 1, blue: 0.09737191632, alpha: 1)
     }
